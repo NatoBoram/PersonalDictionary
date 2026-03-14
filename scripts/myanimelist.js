2a0d30ffc1c4e20b0d935376d2d4cc9dc58e0ab2
Nato Boram <nato.boram+git@gmail.com>
2026-03-13 22:46:43 -0400

    👽️ Update my MyAnimeList

--- scripts/myanimelist.js
+++ scripts/myanimelist.js
@@ -1,5 +1,15 @@
-// https://myanimelist.net/animelist/NatoBoram
-Array.from(document.querySelectorAll("td.data.title.clearfix a.link.sort"))
-	.map((a) => a.textContent)
-	.sort()
-	.join("\n")
+/** https://myanimelist.net/animelist/NatoBoram */
+;(() => {
+	/** @type NodeListOf<HTMLAnchorElement> */
+	const anchors = document.querySelectorAll(
+		"td.data.title.clearfix a.link.sort",
+	)
+
+	const tsv = Array.from(anchors)
+		.map((a) => `\t${a.textContent}\t`)
+		.sort()
+		.join("\n")
+
+	const gboard = `# Gboard Dictionary version:1\n${tsv}`
+	copy(gboard)
+})()
